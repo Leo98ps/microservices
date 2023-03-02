71e4a455f5b42ec3f914d8e1f4663bbf18ba9d3f
Leonardo Pereira <leo.leo.5555@live.com>
2023-03-02 20:56:51 -0300

    Initial commit

---
+++ src/main/java/br/com/projeto/Greeting.java
@@ -0,0 +1,29 @@
+package br.com.projeto;
+
+public class Greeting {
+
+	private String nome;
+	
+	private Long numero;
+
+	public Greeting(String nome, Long numero) {
+		this.nome = nome;
+		this.numero = numero;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+}
